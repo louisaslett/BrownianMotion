5fca4dc7605467f11165c0fa748b32d9fe63a062
Louis Aslett <louis.aslett@durham.ac.uk>
2019-07-31 23:25:32 +0100

    Add Rmpfr dependency.

--- R/util.R
+++ R/util.R
@@ -1,5 +1,6 @@
 #' @importFrom tibble tibble add_row
 #' @importFrom glue glue
+#' @importFrom Rmpfr mpfr
 
 is.realscalar <- function(x) {
   is.atomic(x) && (length(x) == 1L) && !is.character(x) && !is.raw(x) && !is.logical(x) && (Im(x) == 0)
