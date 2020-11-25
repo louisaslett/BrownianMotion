f33bfdb60ca066c7093ca7eb74057ba8b1872544
Louis Aslett <louis.aslett@durham.ac.uk>
2020-11-25 16:12:03 +0000

    Add first Bessel layers implementation

--- R/bessel_layers.R
+++ R/bessel_layers.R
@@ -1,13 +1,16 @@
 #' Construct Bessel layers
 #'
-#' Unbiasedly simulates the smallest Bessel layers which contains the Brownian
-#' motion between two known sample points by retrospective Bernoulli sampling
-#' and inversion sampling.
+#' Performs unbiased simulation of the smallest Bessel layers which contain the
+#' Brownian motion between two known sample points by retrospective Bernoulli
+#' sampling and inversion sampling.
 #'
 #' @param bm a Brownian motion object from which simulation should continue.
-#'   Note the object is updated in place.
+#'   Note the object is updated in place
 #' @param s left hand time point
 #' @param t right hand time point
+#' @param mult the default layer size is \code{sqrt((t-s)/2)}.  You can scale
+#'   this by specifying the \code{mult} argument which will result in layer
+#'   sizes of \code{mult*sqrt((t-s)/2)}.
 #'
 #' @return
 #' The Brownian motion object which was passed in argument \code{bm} is
@@ -15,13 +18,21 @@
 #' dplyr (and other) style pipes.
 #'
 #' @export
-bessel.layers <- function(bm, s, t) {
+bessel.layers <- function(bm, s, t, mult = 1) {
   if(!("BrownianMotion" %in% class(bm))) {
     stop("bm argument must be a BrownianMotion object.")
   }
-  if(!is.realscalar(s) || !is.realscalar(t)) {
-    stop("s and t must be real scalars.")
+  if(!is.realscalar(s) || !is.realscalar(t) || !is.realscalar(mult)) {
+    stop("s, t and mult must be real scalars.")
   }
+  if(s >= t) {
+    stop("s must be less than t.")
+  }
+  if(mult <= 0) {
+    stop("mult must be strictly positive.")
+  }
+
+  # Are these endpoints part of the skeleton?
   if(!(s %in% bm$t)) {
     stop("s is not a known time point in the supplied Brownian motion object.")
   }
@@ -29,10 +40,37 @@ bessel.layers <- function(bm, s, t) {
     stop("t is not a known time point in the supplied Brownian motion object.")
   }
 
-  invisible(bessel.layers_(bm, s, t))
+  # Make sure no part of [s,t] interval lies inside any other layer
+  # if(any(bm$bounds$t.l>s & bm$bounds$t.u<t) || any(bm$bessel.layers$t.l>s & bm$bessel.layers$t.u<t)) {
+  #   stop("Cannot create bessel layer within another layer specification.")
+  # }
+
+  # Find all intervals between s and t not currently inside any other layer
+  s.i <- which(s == bm$t)
+  t.i <- which(t == bm$t)
+  all.pairs <- matrix(c(bm$t[s.i:(t.i-1)], bm$t[(s.i+1):t.i]), byrow = FALSE, ncol = 2)
+  mid <- (all.pairs[,2]+all.pairs[,1])/2
+  incl <- rep(TRUE, nrow(all.pairs))
+  for(i in 1:length(mid)) {
+    if(any(mid[i] >= bm$bounds$t.l & mid[i] <= bm$bounds$t.u) ||
+       any(mid[i] >= bm$bessel.layers$t.l & mid[i] <= bm$bessel.layers$t.u)) {
+      incl[i] <- FALSE
+    }
+  }
+  all.pairs <- all.pairs[incl,,drop = FALSE]
+
+  if(nrow(all.pairs) == 0) {
+    stop("No intervals in the skeleton between s and t found that do not have a layer specification already.")
+  }
+
+  for(i in 1:nrow(all.pairs)) {
+    bessel.layers_(bm, all.pairs[i,1], all.pairs[i,2], mult)
+  }
+
+  invisible(bm)
 }
 
-bessel.layers_ <- function(bm, s, t) {
+bessel.layers_ <- function(bm, s, t, mult) {
   res <- bessel.layers.sim_(bm, s, t, bm$W_t[match(s, bm$t)], bm$W_t[match(t, bm$t)])
 
   bm$bessel.layers <- add_row(bm$bessel.layers,
@@ -46,10 +84,10 @@ bessel.layers_ <- function(bm, s, t) {
   bm
 }
 
-bessel.layers.sim_ <- function(bm, s, t, x, y) {
+bessel.layers.sim_ <- function(bm, s, t, x, y, mult = 1) {
   xb <- min(x,y)
   yb <- max(x,y)
-  adf <- sqrt((t-s)/2)
+  adf <- sqrt((t-s)/2) * mult
   act <- 1
   gind <- 0
   u1 <- runif(1,0,1)
--- R/util_maths.R
+++ R/util_maths.R
@@ -1,4 +1,5 @@
 mpfrthr <- 9 # m threshold for high precision
+mpfrpbn <- 10
 
 eagammaC_ <- function(m, s, t, x, y, L, U) {
   if(m >= mpfrthr) {
@@ -31,5 +32,128 @@ eazetaC_ <- function(m, s, t, x, y, L, U) {
     s1 <- s2 + exp(P*((m+1)/2)^2*D^2 - P*((m+1)/2)*D*z) +
       exp(P*((m+1)/2)^2*D^2 + P*((m+1)/2)*D*z)
   }
-  c(s1=s2,s2=s2)
+  c(s1=s1,s2=s2)
 }
+
+##########
+
+eazeta_ <- function(n, s, t, x, y, L, U) {
+  if(max(x-U, y-U, L-x, L-y) >= 0) {
+    1
+  } else {
+    j <- 1:(ceiling(n/2))
+    P <- -2/(t-s)
+    D <- U-L
+    D1 <- D*j + L
+    D2 <- D*j - U
+    z <- y-x
+    if(n%%2 == 0) {
+      sum(exp(P*(D1-x)*(D1-y)) +
+            exp(P*(D2+x)*(D2+y)) -
+            exp(P*j^2*D^2-P*j*D*z) -
+            exp(P*j^2*D^2+P*j*D*z))
+    } else {
+      sum(exp(P*(D1-x)*(D1-y)) +
+            exp(P*(D2+x)*(D2+y))) -
+        sum(exp(P*j[1:length(j)-1]^2*D^2 - P*j[1:length(j)-1]*D*z) +
+              exp(P*j[1:length(j)-1]^2*D^2 + P*j[1:length(j)-1]*D*z))
+    }
+  }
+}
+
+eagamma_ <- function(n, s, t, x, y, L, U) {
+  1 - eazeta_(n, s, t, x, y, L, U)
+}
+
+eapsi_ <- function(j, s, t, m, xoy, u) {
+  P <- -2*abs(u-m)*j/(t-s)
+  (2*abs(u-m)*j - (xoy-m)) * exp(P*(abs(u-m)*j - (xoy-m)))
+}
+
+eachi_ <- function(j, s, t, m, xoy, u) {
+  P <- -2*abs(u-m)*j/(t-s)
+  (2*abs(u-m)*j + (xoy-m)) * exp(P*(abs(u-m)*j + (xoy-m)))
+}
+
+eadel2_ <- function(n, s, t, m, xoy, u) {
+  if(max(xoy-u, m-xoy) >= 0) {
+    0
+  } else {
+    if(n%%2 == 0) {
+      j <- 1:(n/2)
+      1-(sum(eapsi_(j, s, t, m, xoy, u) - eachi_(j, s, t, m, xoy, u)))/(xoy-m)
+    } else {
+      if(n>1) {
+        j <- 1:((n-1)/2)
+        1-(sum(eapsi_(j, s, t, m, xoy, u) - eachi_(j, s, t, m, xoy, u)))/(xoy-m) - eapsi_(max(j)+1, s, t, m, xoy, u)/(xoy-m)
+      } else {
+        1-eapsi_(1, s, t, m, xoy, u)/(xoy-m)
+      }
+    }
+  }
+}
+
+eadelR_ <- function(n, s, t, x, y, m, u) {
+  if(x == m) {
+    xI <- 1
+  } else {
+    xI <- 0
+  }
+  if(y == m) {
+    yI <- 1
+  } else {
+    yI <- 0
+  }
+  if(max(xI, yI) == 1) {
+    delT <- 2
+  } else {
+    delT <- 1
+  }
+  if(m > min(x,y)) {
+    x <- -x
+    y <- -y
+    m <- -m
+    u <- -u
+  }
+  if(max(x-u, y-u, m-x, m-y) >= 0) {
+    out <- 0
+  }
+  if(delT == 1) {
+    out <- eagamma_(n, s, t, x, y, m, u)/(1 - exp(-2*(x-m)*(y-m)/(t-s)))
+  }
+  if(delT == 2) {
+    if(xI*yI == 0) {
+      xoy <- max(x,y)
+      out <- eadel2_(n, s, t, m, xoy, u)
+    } else {
+      out <- 0
+    }
+  }
+  if(out < 0) {
+    out <- 0
+  }
+  if(out > 1) {
+    out <- 1
+  }
+  if((t-s) == 0) {
+    out <- 1
+  }
+  out
+}
+
+eadelC_ <- function(mt, s, t, x, y, m, u) {
+  if(mt >= mpfrthr) {
+    pbn <- mt*mpfrpbn
+    s <- mpfr(s, precBits = pbn)
+    t <- mpfr(t, precBits = pbn)
+    x <- mpfr(x, precBits = pbn)
+    y <- mpfr(y, precBits = pbn)
+    m <- mpfr(m, precBits = pbn)
+    u <- mpfr(u, precBits = pbn)
+    c(s1 = eadelR_(mt, s, t, x, y, m, u),
+      s2 = eadelR_(mt+1, s, t, x, y, m, u))
+  } else {
+    scale:::eadel_pair_cpp(mt,s,t,x,y,m,u)
+  }
+}
+
