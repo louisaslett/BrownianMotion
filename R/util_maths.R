e80e8f39b0d97e6127303a976d20d5d5520f307e
Louis Aslett <louis.aslett@durham.ac.uk>
2021-02-10 17:54:46 +0000

    Add statmod dependency and push out MPFR usage threshold

--- R/util_maths.R
+++ R/util_maths.R
@@ -1,4 +1,4 @@
-mpfrthr <- 9 # m threshold for high precision
+mpfrthr <- 100 # m threshold for high precision
 mpfrpbn <- 10
 
 eagammaC_ <- function(m, s, t, x, y, L, U) {
@@ -223,7 +223,7 @@ eadelC_ <- function(mt, s, t, x, y, m, u) {
 eaminmax_ <- function(s,t,x,y,Ll,Lu,Ul,Uu){
   if(rbinom(1,1,0.5)==1){minI<-1}else{minI<--1; x<--x;y<--y;Ll<--Uu;Lu<--Ul}
   u1<-runif(1,exp(-2*(Ll-x)*(Ll-y)/(t-s)),exp(-2*(Lu-x)*(Lu-y)/(t-s))); u2<-runif(1,0,1); m<-x-0.5*(sqrt((y-x)^2-2*(t-s)*log(u1))-(y-x))
-  c1 <- (y-m)^2/(2*(t-s)); c2 <- (m-x)^2/(2*(t-s)); I1<-rinvgauss(1,sqrt(c1/c2),2*c1);I2<-1/rinvgauss(1,sqrt(c2/c1),2*c2);V<-if(runif(1,0,1)<=(1/(1+sqrt(c1/c2)))){I1}else{I2}; tau <- (s*V+t)/(1+V)
+  c1 <- (y-m)^2/(2*(t-s)); c2 <- (m-x)^2/(2*(t-s)); I1<-statmod::rinvgauss(1,sqrt(c1/c2),2*c1);I2<-1/statmod::rinvgauss(1,sqrt(c2/c1),2*c2);V<-if(runif(1,0,1)<=(1/(1+sqrt(c1/c2)))){I1}else{I2}; tau <- (s*V+t)/(1+V)
   if(minI==-1){m<--m};list(m=m,tau=tau,minI=minI)}
 
 eabesmid_ <- function(q,s,tau,t,x,m,y,minI){
