d36ab82af46ac52c01f15c20a8887865e12d4c49
Louis Aslett <louis.aslett@durham.ac.uk>
2021-11-03 19:04:08 +0000

    Start on multidimensions!  Mostly just careful error checking in create.bm at the moment.

--- R/create_bm.R
+++ R/create_bm.R
@@ -10,24 +10,117 @@
 #' @param prefer prefer is
 #'
 #' @export
-create.bm <- function(t = 0, W_t = 0, refine = TRUE, mult = 1, prefer = "bessel") {
-  if(!is.numeric(t)) {
-    stop("Argument t must be a numeric vector")
-  }
-  if(!is.numeric(W_t)) {
-    stop("Argument W_t must be a numeric vector")
-  }
-  if(length(refine) != 1 || !is.logical(refine)) {
-    stop("refine must be a scalar logical value")
-  }
-  if(length(mult) != 1 || !is.numeric(mult) || mult <= 0) {
-    stop("mult must be a strictly positive scalar")
-  }
-  if(length(prefer) != 1 || !(prefer %in% c("bessel", "intersection"))) {
-    stop("prefer must be one of 'bessel' or 'intersection'")
+create.bm <- function(t = 0, W_t = 0, dim = 1, cov = 1, refine = TRUE, mult = 1, prefer = "bessel") {
+  # t checks
+  if(!isTRUE(e <- checkmate::check_vector(t, strict = TRUE, any.missing = FALSE, min.len = 1, unique = TRUE, null.ok = FALSE))) {
+    stop("Error with argument t: ", e)
   }
 
-  create.bm_(t, W_t, refine, mult, prefer)
+  # dim checks
+  if(!isTRUE(e <- checkmate::check_int(dim, lower = 1))) {
+    stop("Error with argument dim: ", e)
+  }
+  dim <- as.integer(dim)
+
+  # W_t checks
+  if(dim == 1L) {
+    if(!isTRUE(e <- checkmate::check_vector(W_t, strict = TRUE, any.missing = FALSE, len = length(t), null.ok = FALSE))) {
+      stop("Error with argument W_t: ", e)
+    }
+  } else if(dim > 1L) {
+    if(length(t) == 1) { # allow W_t to be scalar or vector
+      if(is.matrix(W_t) && nrow(W_t) == 1) {
+        W_t <- W_t[1,]
+      }
+      if(!isTRUE(e <- checkmate::check_vector(W_t, strict = TRUE, any.missing = FALSE, null.ok = FALSE))) {
+        stop("Error with argument W_t: ", e)
+      }
+      if(length(W_t) == 1) {
+        W_t <- rep(W_t, dim)
+      } else if(length(W_t) != dim) {
+        stop("Error with argument W_t: must be of length 1 or of length dim")
+      }
+      W_t <- matrix(W_t, nrow = 1)
+    } else { # W_t must be matrix
+      if(!isTRUE(e <- checkmate::check_matrix(W_t, any.missing = FALSE, nrows = length(t), ncols = dim))) {
+        stop("Error with argument W_t: ", e)
+      }
+    }
+  }
+
+  # cov checks
+  if(is.matrix(cov)) {
+    if(!isTRUE(e <- checkmate::check_matrix(cov, any.missing = FALSE, nrows = dim, ncols = dim))) {
+      stop("Error with argument cov: ", e)
+    }
+    if(!isSymmetric(cov)) {
+      stop("Error with argument cov: the covariance matrix is not symmetric")
+    }
+    e <- eigen(cov, symmetric = TRUE)
+    if(any(e$values <= 0)) {
+      stop("Error with argument cov: the covariance matrix is not positive definite")
+    }
+  } else {
+    if(!isTRUE(e <- checkmate::check_scalar(cov))) {
+      stop("Error with argument cov: must be either a scalar or matrix (dim x dim)")
+    }
+    if(cov <= 0) {
+      stop("Error with argument cov: scalar cov must be strictly positive")
+    }
+    cov <- diag(cov, nrow = dim, ncol = dim)
+  }
+
+  # refine, mult, prefer checks
+  if(!isTRUE(e <- checkmate::check_logical(refine, any.missing = FALSE))) {
+    stop("Error with argument refine: ", e)
+  }
+  if(length(refine) != 1 && length(refine) != dim) {
+    stop("Error with argument refine: must be length 1 or dim")
+  }
+  if(length(refine) == 1) {
+    refine <- rep(refine, dim)
+  }
+
+  if(!isTRUE(e <- checkmate::check_numeric(mult, finite = TRUE, any.missing = FALSE))) {
+    stop("Error with argument mult: ", e)
+  }
+  if(any(mult <= 0)) {
+    stop("Error with argument mult: must be strictly positive")
+  }
+  if(length(mult) != 1 && length(mult) != dim) {
+    stop("Error with argument mult: must be length 1 or dim")
+  }
+  if(length(mult) == 1) {
+    mult <- rep(mult, dim)
+  }
+
+  if(!isTRUE(e <- checkmate::check_character(prefer, any.missing = FALSE))) {
+    stop("Error with argument prefer: ", e)
+  }
+  prefer <- tolower(prefer)
+  if(any(!(prefer %in% c("bessel", "intersection")))) {
+    stop("Error with argument prefer: must be one of 'bessel' or 'intersection'")
+  }
+  if(length(prefer) != 1 && length(prefer) != dim) {
+    stop("Error with argument prefer: must be length 1 or dim")
+  }
+  if(length(prefer) == 1) {
+    prefer <- rep(prefer, dim)
+  }
+
+
+  bm <- new.env(parent = emptyenv())
+  bm$chol <- chol(cov)
+  bm$dim <- dim
+  bm$Z.bm <- list()
+  for(d in 1:dim) {
+    bm$Z.bm[[d]] <- create.bm_(t, W_t[,d], refine[d], mult[d], prefer[d])
+  }
+  # Add storage for outer path and layers containing the post simulation
+  # transformed values (decided against compute on demand for this)
+  class(bm) <- "BrownianMotionNd"
+
+  bm
 }
 
 create.bm_ <- function(t, W_t, refine, mult, prefer, nested = FALSE) {
