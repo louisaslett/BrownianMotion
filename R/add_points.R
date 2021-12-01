9852464afd68593f6a84e9b1c0de132cd946d496
Louis Aslett <louis.aslett@durham.ac.uk>
2021-12-01 18:28:19 +0000

    Multidimensionalise add points and segments.

--- R/add_points.R
+++ R/add_points.R
@@ -1,12 +1,67 @@
 #' Manually specify the value of a point in the skeleton
 #'
-#' Only valid outside layers. Cannot be existing time (should delete then add)
+#' Note: Only valid for time points that don't lie within existing layers. Cannot be an existing time within the skeleton (in this case you should delete then add the point).
 #'
-#' @param t time
-#' @param W_t value
+#' @param bm a Brownian motion object from which simulation should continue.
+#'   Note the object is updated in place
+#' @param t vector of fixed times of the Brownian motion.  Defaults to value of 0.
+#' @param W_t matrix of size the length of t (rows) x dimension (column). If dim = 1, a vector of the length of t is acceptable. If dim > 1 and the length of t = 1, then a vector of length 1 is acceptable (corresponding to the same value at every co-ordinate), or length d is acceptable (corresponding to the d components). Defaults to 0 for every component.
+#' @param label vector of length 1 or the length of t indicating the introduction of user specified labels (if any). By default, uses any names in the vector of times.
 #'
 #' @export
 add.points <- function(bm, t, W_t, label = names(t)) {
+  UseMethod("add.points")
+}
+
+#' @export
+add.points.BrownianMotionNd <- function(bm, t, W_t, label = names(t)) {
+  if(!("BrownianMotionNd" %in% class(bm))) {
+    stop("bm argument must be a BrownianMotionNd object.")
+  }
+
+  # t checks
+  if(!isTRUE(e <- checkmate::check_vector(t, strict = TRUE, any.missing = FALSE, min.len = 1, unique = TRUE, null.ok = FALSE))) {
+    stop("Error with argument t: ", e)
+  }
+
+  # W_t checks
+  if(bm$dim == 1L) {
+    if(!isTRUE(e <- checkmate::check_vector(W_t, strict = TRUE, any.missing = FALSE, len = length(t), null.ok = FALSE))) {
+      stop("Error with argument W_t: ", e)
+    }
+    if(!is.matrix(W_t)) {
+      W_t <- matrix(W_t, ncol = 1)
+    }
+  } else if(bm$dim > 1L) {
+    if(length(t) == 1) { # allow W_t to be scalar or vector
+      if(is.matrix(W_t) && nrow(W_t) == 1) {
+        W_t <- W_t[1,]
+      }
+      if(!isTRUE(e <- checkmate::check_vector(W_t, strict = TRUE, any.missing = FALSE, null.ok = FALSE))) {
+        stop("Error with argument W_t: ", e)
+      }
+      if(length(W_t) == 1) {
+        W_t <- rep(W_t, bm$dim)
+      } else if(length(W_t) != bm$dim) {
+        stop("Error with argument W_t: must be of length 1 or of length dim")
+      }
+      W_t <- matrix(W_t, nrow = 1)
+    } else { # W_t must be matrix
+      if(!isTRUE(e <- checkmate::check_matrix(W_t, any.missing = FALSE, nrows = length(t), ncols = bm$dim))) {
+        stop("Error with argument W_t: ", e)
+      }
+    }
+  }
+
+  for(d in 1:bm$dim) {
+    add.points(bm$Z.bm[[d]], t, W_t[,d])
+  }
+
+  invisible(bm)
+}
+
+#' @export
+add.points.BrownianMotion <- function(bm, t, W_t, label = names(t)) {
   if(!("BrownianMotion" %in% class(bm))) {
     stop("bm argument must be a BrownianMotion object.")
   }
--- R/add_segment.R
+++ R/add_segment.R
@@ -1,10 +1,107 @@
-#' Create/move a segment
+#' Create / move a segment
 #'
-#' @param l
+#' @param bm a Brownian motion object from which simulation should continue.
+#'   Note the object is updated in place
+#' @param l closed left end of interval to add a segment.  By default, `-Inf` which
+#'     results in the introduction of a segment by shifting the entire path up to `r`
+#' @param r open right end of interval to add a segment.  By default, `Inf` which
+#'     results in the introduction of a segment by shifting the entire path from `l`
+#' @param W_t vector of length the dimension of the Brownian motion specifying the coordinate of the Brownian motion at the time l (specified by the user). A vector of length 1 is acceptable (corresponding to the same value at every co-ordinate). Defaults to NULL, meaning this argument is ignored in favor of delta.
+#' @param delta vector of length the dimension of the Brownian motion specifying the shift of the Brownian motion at the time l (specified by the user). A vector of length 1 is acceptable (corresponding to the same shift for every co-ordinate). Defaults to NULL, meaning this argument is ignored in favor of W_t.
+#' @param label vector of length 1 or the length of t indicating the introduction of user specified labels (if any). By default, uses any names in the vector of times.
 #'
 #'
 #' @export
 add.segment <- function(bm, l = -Inf, r = Inf, W_t = NULL, delta = NULL, label = NULL) {
+  UseMethod("add.segment")
+}
+
+#' @export
+add.segment.BrownianMotionNd <- function(bm, l = -Inf, r = Inf, W_t = NULL, delta = NULL, label = NULL) {
+  if(!("BrownianMotionNd" %in% class(bm))) {
+    stop("bm argument must be a BrownianMotionNd object.")
+  }
+
+  if(!is.realscalar(l)) {
+    stop("l must be a scalar.")
+  }
+  if(!is.realscalar(r)) {
+    stop("r must be a scalar.")
+  }
+  if(l >= r) {
+    stop("l must be less than r.")
+  }
+  if(l!=-Inf && !(l %in% bm$Z.bm[[1]]$t)) {
+    stop("l must be a sampled path point")
+  }
+  if(r!=Inf && !(r %in% bm$Z.bm[[1]]$t)) {
+    stop("r must be a sampled path point")
+  }
+  if(is.null(delta) && is.null(W_t)) {
+    stop("One of delta or W_t must be specified")
+  }
+  if(!is.null(delta) && !is.null(W_t)) {
+    stop("Can't specify both delta and W_t, please choose only one")
+  }
+
+  if(!is.null(W_t)) {
+    # W_t checks
+    if(bm$dim == 1L) {
+      if(!isTRUE(e <- checkmate::check_vector(W_t, strict = TRUE, any.missing = FALSE, len = 1, null.ok = FALSE))) {
+        stop("Error with argument W_t: ", e)
+      }
+      if(!is.matrix(W_t)) {
+        W_t <- matrix(W_t, ncol = 1)
+      }
+    } else if(bm$dim > 1L) {
+      if(is.matrix(W_t) && nrow(W_t) == 1) {
+        W_t <- W_t[1,]
+      }
+      if(!isTRUE(e <- checkmate::check_vector(W_t, strict = TRUE, any.missing = FALSE, null.ok = FALSE))) {
+        stop("Error with argument W_t: ", e)
+      }
+      if(length(W_t) == 1) {
+        W_t <- rep(W_t, bm$dim)
+      } else if(length(W_t) != bm$dim) {
+        stop("Error with argument W_t: must be of length 1 or of length dim")
+      }
+      W_t <- matrix(W_t, nrow = 1)
+    }
+  }
+  if(!is.null(delta)) {
+    # delta checks
+    if(bm$dim == 1L) {
+      if(!isTRUE(e <- checkmate::check_vector(delta, strict = TRUE, any.missing = FALSE, len = 1, null.ok = FALSE))) {
+        stop("Error with argument delta: ", e)
+      }
+      if(!is.matrix(delta)) {
+        delta <- matrix(delta, ncol = 1)
+      }
+    } else if(bm$dim > 1L) {
+      if(is.matrix(delta) && nrow(delta) == 1) {
+        delta <- delta[1,]
+      }
+      if(!isTRUE(e <- checkmate::check_vector(delta, strict = TRUE, any.missing = FALSE, null.ok = FALSE))) {
+        stop("Error with argument delta: ", e)
+      }
+      if(length(delta) == 1) {
+        delta <- rep(delta, bm$dim)
+      } else if(length(delta) != bm$dim) {
+        stop("Error with argument delta: must be of length 1 or of length dim")
+      }
+      delta <- matrix(delta, nrow = 1)
+    }
+  }
+
+  for(d in 1:bm$dim) {
+    add.segment(bm$Z.bm[[d]], l, r, W_t[,d], delta[,d], label)
+  }
+
+  invisible(bm)
+}
+
+#' @export
+add.segment.BrownianMotion <- function(bm, l = -Inf, r = Inf, W_t = NULL, delta = NULL, label = NULL) {
   if(!("BrownianMotion" %in% class(bm))) {
     stop("bm argument must be a BrownianMotion object.")
   }
